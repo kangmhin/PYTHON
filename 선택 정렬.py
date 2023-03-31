de475afe1d40c41b2f2cb108c6c2e6ed94886951
kangmhin <109192011+kangmhin@users.noreply.github.com>
2023-03-31 16:00:13 +0900

    Delete 선택 정렬.py

--- 선택 정렬.py
+++
@@ -1,9 +0,0 @@
-
-arraysort = list(map(str, input("정수 입력 : ").split()))
-n = len(arraysort)
-for i in range(n):
-     for j in range(0, n-i-1):
-         if arraysort[j]>arraysort[j+1]:
-             arraysort[j], arraysort[j+1] = arraysort[j+1], arraysort[j]
-for i in range(n):
-    print(arraysort[i], end = " ")
